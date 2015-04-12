b00bb40be5367b4b94691c3980dbe8ebd4ee07c5
79257 <79257@163.com>
2015-04-12 09:48:09 +0800

    代码格式化
    2015年4月12日

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <footer role="contentinfo" class="footer">
   <p><small>© 2015. All Rights Reserved.</small></p>
-  <p><small><a href="http://ghostium.oswaldoacauan.com/" target="_blank">Heiteo Theme</a> by <a href="http://nyf.pw/" target="_blank">nyf.pw</a></small></p>
+  <p><small><a href="http://nyf.pw/theme.html" target="_blank">Heiteo Theme</a> by <a href="http://nyf.pw/" target="_blank">nyf.pw</a></small></p>
   <p><small>Proudly published with <a href="http://typecho.org/" target="_blank">Typecho</a></small></p>
 </footer>
 </div>
--- sidebar.php
+++ sidebar.php
@@ -7,11 +7,11 @@
       <li class="drawer-list-item"> <a href="<?php $this->options->siteUrl(); ?>" data-pjax> <i class="fa fa-home"></i>首页 </a> </li>
       <li class="drawer-list-item"> <a href="/me.html" target="_blank"> <i class="fa fa-info"></i>关于 </a> </li>
       <li class="drawer-list-item"> <a href="/theme.html" target="_blank"> <i class="fa fa-file-code-o"></i>主题 </a> </li>
-      <li class="drawer-list-item"> <a href="/theme.html" target="_blank"> <i class="fa fa-inbox"></i>项目 </a> </li>
+      <li class="drawer-list-item"> <a href="/project.html" target="_blank"> <i class="fa fa-inbox"></i>项目 </a> </li>
       <li class="drawer-list-item"> <a href="mailto:79257@163.com" target="_blank"> <i class="fa fa-envelope"></i>联系 </a> </li>
       <li class="drawer-list-divider"></li>
       <li class="drawer-list-item drawer-list-title"> Follow me </li>
-      <li class="drawer-list-item"> <a href="http://weibo.com/n79257" target="_blank"> <i class="fa fa-weibo"></i>新浪微博 </a> </li>
+      <li class="drawer-list-item"> <a href="https://git.oschina.net/79257 target="_blank"> <i class="fa fa-git"></i>Git@OSC </a> </li>
       <li class="drawer-list-item"> <a href="https://github.com/79257" target="_blank"> <i class="fa fa-github"></i>Github </a> </li>
       <li class="drawer-list-item"> <a href="http://wpa.qq.com/msgrd?v=3&uin=604163270&site=qq&menu=ye" target="_blank"> <i class="fa fa-qq"></i>QQ </a> </li>
     </ul>
