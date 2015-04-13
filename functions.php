05e631b3fd3035b8ac246bd686643bc3c4f62146
79257 <79257@163.com>
2015-04-13 09:54:59 +0800

    1.2
    2015年4月13日

--- functions.php
+++ functions.php
@@ -11,6 +11,17 @@ function themeConfig($form) {
 	//设置侧边栏样式
 	//$sidebarstyle = new Typecho_Widget_Helper_Form_Element_Radio('sidebarstyle', array('展开' => 'post-template tag-getting-started  pace-done drawer-open drawer-transition','折叠' => 'post-template tag-getting-started'), NULL, _t('侧边栏样式'), _t('设置侧边栏的默认样式'));
     //$form->addInput($sidebarstyle);
+	//设置多说ID
+	$duoshuoId = new Typecho_Widget_Helper_Form_Element_Text('duoshuoId', NULL, NULL, _t('你在多说的域名'), _t('只需要输入二级名称即可（比如：nyf.duoshuo.com,那么就输入nyf）'));
+    $form->addInput($duoshuoId);
+	
+	//多说评论
+	$duoshuo = new Typecho_Widget_Helper_Form_Element_Checkbox('duoshuo', 
+    array('PostShowDuoshuo' => _t('文章页是否启用多说评论'),
+    'PageShowDuoshuo' => _t('独立页面是否启用多说评论')),
+    array('PostShowDuoshuo', 'PageShowDuoshuo'), _t('多说评论（注意，如果不开启多说评论，则页面不会有默认评论系统）'));
+    
+    $form->addInput($duoshuo->multiMode());
 
 }
 ?>
